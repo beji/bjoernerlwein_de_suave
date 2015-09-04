1e6bd3b9ad48dc198d72975922e19ad2e5a2b5bf
beji <bjoernerlwein@gmail.com>
2015-09-04 22:45:52 +0200

    update build script

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 mono .paket/paket.bootstrapper.exe
 mono .paket/paket.exe install
-mono packages/FAKE/tools/FAKE.exe buildscripts/release.fsx
+mono packages/FAKE/tools/FAKE.exe buildscripts/release.fsx BuildRelease
+cd build
+mono bjoernerlwein_de.exe production
