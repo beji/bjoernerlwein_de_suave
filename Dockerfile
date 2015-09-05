f8cea93e8920e5c4abe9a9c4f5750ef9d79a737f
beji <bjoernerlwein@gmail.com>
2015-09-05 20:02:37 +0200

    use less to compile the css

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM geoder101/fsharp-extended
+RUN npm install -g less
 RUN mkdir /app
 WORKDIR /app
 ADD . /app
--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 mono .paket/paket.bootstrapper.exe
 mono .paket/paket.exe install
 bower install --allow-root
+lessc bjoernerlwein_de/static/css/style.less > bjoernerlwein_de/static/css/style.css
 mono packages/FAKE/tools/FAKE.exe buildscripts/release.fsx BuildRelease
 cd build
 mono bjoernerlwein_de.exe production
