3dd5a43717f085e6ecb2c44e2e8a60cdd5006641
beji <bjoernerlwein@gmail.com>
2016-01-05 20:52:37 +0100

    fix StaticfileRoutes

--- bjoernerlwein_de/StaticfileRoutes.fs
+++ bjoernerlwein_de/StaticfileRoutes.fs
@@ -1,21 +1,19 @@
 ﻿module StaticfileRoutes
 
-
 open Suave
-open Suave.Http
-open Suave.Http.Applicatives
-open Suave.Http.RequestErrors
-open Suave.Utils
+open Suave.RequestErrors
+open Suave.Files
+open Suave.Filters
 open System.IO
-open Suave.Types
+open Suave.Operators
 
 let routes =
     choose [
-        GET >>= path "/js/vue.js" >>= Files.file "./static/bower/vue/dist/vue.js"
-        GET >>= path "/js/nanoajax.js" >>= Files.file "./static/bower/nanoajax/nanoajax.min.js"
-        GET >>= path "/js/script.js" >>= Files.file "./static/js/script.js"
-        GET >>= path "/js/script.min.js" >>= Files.file "./static/js/script.min.js"
-        GET >>= path "/css/normalize.css" >>= Files.file "./static/bower/normalize-css/normalize.css"
+        GET >=> path "/js/vue.js" >=> Files.file "./static/bower/vue/dist/vue.js"
+        GET >=> path "/js/nanoajax.js" >=> Files.file "./static/bower/nanoajax/nanoajax.min.js"
+        GET >=> path "/js/script.js" >=> Files.file "./static/js/script.js"
+        GET >=> path "/js/script.min.js" >=> Files.file "./static/js/script.min.js"
+        GET >=> path "/css/normalize.css" >=> Files.file "./static/bower/normalize-css/normalize.css"
         pathScan "/css/%s" (fun (file) ->
             let path = "./static/css/" + file
             match File.Exists path with
